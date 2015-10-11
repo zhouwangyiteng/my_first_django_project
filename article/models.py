22beb9c646e91d72bac386500abdb429da01c07b
zhouwangyiteng <zhouwang122@qq.com>
2015-10-11 21:07:46 +0800

    add duoshuo an markdown

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -6,6 +7,10 @@ class Article(models.Model):
 	category = models.CharField(max_length = 100, blank = True)
 	date_time = models.DateTimeField(auto_now_add = True)
 	content = models.TextField(blank = True, null = True)
+
+	def get_absolute_url(self):
+		path = reverse('detail', kwargs={'id':self.id})
+		return "http://127.0.0.1:8000%s" % path
 
 	def __unicode__(self):
 		return self.title
