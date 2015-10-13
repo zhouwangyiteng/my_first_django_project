e0fcd567ca8448bcc900e5ed962d009b3ece1848
zhouwangyiteng <zhouwang122@qq.com>
2015-10-13 23:06:27 +0800

    add aboutMe and tag

--- article/views.py
+++ article/views.py
@@ -24,3 +24,13 @@ def archives(request) :
 	except Article.DoesNotExist :
 		raise Http404
 	return render(request, 'archives.html', {'post_list' : post_list,  'error' : False})
+
+def about_me(request):
+	return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+	try:
+		post_list = Article.objects.filter(category__iexact = tag) 
+	except Article.DoesNotExist :
+		raise Http404
+	return render(request, 'tag.html', {'post_list' : post_list})
